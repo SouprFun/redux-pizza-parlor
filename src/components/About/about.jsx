7d61f11f3421b7bf20037c8a2c491732dd059a66
Ujjwal Adhikari <ujjwalnd@Ujjwals-MacBook-Pro.local>
2022-06-02 11:16:31 -0500

    completed links

---
+++ src/components/About/about.jsx
@@ -0,0 +1,15 @@
+function About(){
+    return(
+        <>
+        <h1>
+        PURPOSE! VALUES!
+        </h1>
+        <h2>
+  Prime Pizza believes in doing the right thing, putting people first, creating inspired solutions, championing our customers, and growing and winning together.  
+
+        </h2>
+    </>
+    )
+}
+
+export default About;
