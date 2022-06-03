fffc1fcea7678448b0ca40ee905bba8802b5b249
Brian Campbell <bry123ftw@gmail.com>
2022-06-03 11:49:45 -0500

    stuff from phillipe that wasn't pushing

--- src/components/CheckoutItem/CheckoutItem.jsx
+++ src/components/CheckoutItem/CheckoutItem.jsx
@@ -1,16 +1,30 @@
-function CheckoutItem() {
+import { useSelector } from "react-redux";
 
+function CheckoutItem() {
+    const customer = useSelector(store => store.customer);
+    const pizza = useSelector(store => store.cart);
+    console.log(pizza);
+    function chekoutButton(customer) {
+        if (customer.delivery === true) {
+            return ("for Delivery");
+        } else {
+            return ('for Pickup');
+        }
+    }
     return (
         <>
-            <td>
-                {pizza.name}
-            </td>
-            <td>
-                <span>{pizza.price}</span>
-            </td>
-
+            <div>
+                <h3>{customer.customer_name}</h3>
+                <h3>{customer.costumer_address}</h3>
+                <h3>{customer.city}</h3>
+                <h3>{customer.zip}</h3>
+                <h3>{chekoutButton}</h3>
+            </div>
+            {pizza.map((pizzas, index) =>
+                <li id="pizza" key={index}>{pizzas.name}
+                    {pizzas.price}</li>
+            )}
         </>
     )
 }
-
 export default CheckoutItem;
